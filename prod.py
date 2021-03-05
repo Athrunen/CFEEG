f852ce028bad9dbc3fd26ec55e6a40e4febeb8d8
Malte Vrampe <athrunen@gmail.com>
2021-03-05 10:21:58 +0100

    Allowed for execution via GUI

--- prod.py
+++ prod.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor
